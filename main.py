61208bb03ee98a28abbf6484b9bf7a17a2dc7ed4
Moshe Winberg (slash) <MosheWinberg@gmail.com>
2026-03-29 20:05:06 +0300

    feat: implement ProfilePage class to manage user book quantities
    Signed-off-by: Moshe Winberg (slash) <MosheWinberg@gmail.com>

--- main.py
+++ main.py
@@ -1,38 +1,21 @@
 from pages.book_page import BookPage
 from pages.home_page import HomePage
+from pages.profile_page import ProfilePage
 from pages.search_results_page import SearchResultsPage
 import time
 
 
 if __name__ == "__main__":
-    # home_page = HomePage()
-    # home_page.take_screenshot("home_page")
+    home_page = HomePage()
+    home_page.take_screenshot("home_page")
 
-    # search_results_page = home_page.search_books_by_title_under_year(
-    #     "The Great Gatsby", 2026)
+    search_results_page = home_page.search_books_by_title_under_year(
+        "The Great Gatsby", 2026)
 
-    # books = search_results_page.get_books_urls(limit=35)
-
-    # print(len(books), books)
-
-    book_page = BookPage(
-        "https://openlibrary.org/works/OL468431W/The_Great_Gatsby?edition=key%3A/books/OL35657482M")
-
-    book_page.set_book_as_want_to_read()
-    time.sleep(2)
-    book_page.set_book_as_already_read()
-    time.sleep(2)
-    book_page.set_book_as_want_to_read()
-    time.sleep(2)
-    book_page.set_book_as_currently_reading()
-    time.sleep(2)
-    book_page.set_book_as_want_to_read()
-    time.sleep(2)
-    book_page.set_book_as_already_read()
-    time.sleep(2)
-    book_page.set_book_as_currently_reading()
-    time.sleep(2)
-    book_page.set_book_as_want_to_read()
-    time.sleep(2)
-    book_page.set_book_as_currently_reading()
-    time.sleep(2)
+    books = search_results_page.get_books_urls(limit=35)
+    
+    
+    profile_page = ProfilePage()
+    print("Want to Read quantity:", profile_page.get_want_to_read_quantity())
+    print("Currently Reading quantity:", profile_page.get_currently_reading_quantity())
+    print("Already Read quantity:", profile_page.get_already_read_quantity())
