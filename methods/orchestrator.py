18cda47a092dbdc87a28ccbce5126652197eff57
Moshe Winberg (slash) <MosheWinberg@gmail.com>
2026-04-07 00:20:21 +0300

    Propagate title context through orchestrator and reading-list assertions

--- methods/orchestrator.py
+++ methods/orchestrator.py
@@ -1,10 +1,13 @@
 
 from helpers.configs import Config
 from helpers.logger import print_info
+from helpers.screenshots_taker import ScreenshotsTaker
 from methods.add_books_to_reading_list import add_books_to_reading_list
 from methods.assert_reading_list_count import assert_reading_list_count
 from methods.search_books_by_title_under_year import search_books_by_title_under_year
 from pages.inheriting_pages.profile_page import ProfilePage
+
+screenshotsTaker = ScreenshotsTaker()
 
 
 async def orchestrator():
@@ -16,9 +19,10 @@ async def orchestrator():
     profile_page = await ProfilePage.create()
 
     if config.settings.initialize_book_shelves:
+        await profile_page.get_want_and_already_read_quantities("before_initial")
         await profile_page.remove_all_books_from_shelves()
 
-    want_to_read_prev, already_read_prev = await profile_page.get_want_and_already_read_quantities()
+    want_to_read_prev, already_read_prev = await profile_page.get_want_and_already_read_quantities("initial")
 
     for query in config.queries:
         print_info(
@@ -31,4 +35,4 @@ async def orchestrator():
         want_to_read, already_read = await add_books_to_reading_list(urls)
         want_to_read_prev += want_to_read
         already_read_prev += already_read
-        await assert_reading_list_count(want_to_read_prev, already_read_prev)
+        await assert_reading_list_count(want_to_read_prev, already_read_prev, query.query)
