2059284417a27df2372fd24b237715decf38123c
Moshe Winberg (slash) <MosheWinberg@gmail.com>
2026-04-06 15:15:24 +0300

    refactor: extract workflow into methods orchestrator

---
+++ methods/add_books_to_reading_list.py
@@ -0,0 +1,33 @@
+from helpers.logger import print_info
+from pages.inheriting_pages.book_page import BookPage
+
+import random
+
+
+async def add_books_to_reading_list(urls: list[str]):
+    want_to_read = already_read = 0
+    if not urls:
+        return want_to_read, already_read
+
+    book_page = await BookPage.create(urls[0])
+    for index, url in enumerate(urls):
+        if index > 0:
+            book_page.book_url = url
+            await book_page.navigate()
+
+        if random.choice([True, False]):
+            res = await book_page.set_book_as_want_to_read()
+            want_to_read += 1
+            if res == -1:
+                print_info(
+                    f"Book at {url} was already in 'Want to Read' shelf, skipping addition.")
+                already_read -= 1
+        else:
+            res = await book_page.set_book_as_already_read()
+            already_read += 1
+            if res == -1:
+                print_info(
+                    f"Book at {url} was already in 'Already Read' shelf, skipping addition.")
+                want_to_read -= 1
+
+    return want_to_read, already_read
---
+++ methods/assert_reading_list_count.py
@@ -0,0 +1,12 @@
+from helpers.logger import print_info
+from pages.inheriting_pages.profile_page import ProfilePage
+
+
+async def assert_reading_list_count(expected_want_to_read: int, expected_already_read: int):
+    profile_page = await ProfilePage.create()
+    actual_want_to_read = await profile_page.get_want_to_read_quantity()
+    actual_already_read = await profile_page.get_already_read_quantity()
+
+    assert actual_want_to_read == expected_want_to_read and actual_already_read == expected_already_read, f"Expected {expected_want_to_read} books in 'Want to Read' and {expected_already_read} books in 'Already Read', but found {actual_want_to_read} and {actual_already_read}."
+    print_info(
+        f"Assertion passed: {actual_want_to_read} books in 'Want to Read' and {actual_already_read} books in 'Already Read' as expected.")
