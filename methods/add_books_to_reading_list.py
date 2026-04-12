6750c69ff368e5ae99ab91c7b03291098d77fe0b
Moshe Winberg (slash) <MosheWinberg@gmail.com>
2026-04-12 21:18:41 +0300

    fix: update docstring and comments in add_books_to_reading_list for clarity
    Signed-off-by: Moshe Winberg (slash) <MosheWinberg@gmail.com>

--- methods/add_books_to_reading_list.py
+++ methods/add_books_to_reading_list.py
@@ -5,6 +5,12 @@ import random
 
 
 async def add_books_to_reading_list(urls: list[str]):
+    """
+    Adds books to the user's reading list by randomly assigning them to either the "Want to Read" or "Already Read" shelf.
+    It iterates through the provided list of book URLs, navigates to each book's page, and attempts to add it to one of the shelves.
+    If a book is already in one shelf, it skips adding it to the other and adjusts the counters accordingly.
+    The function returns the total count of books added to each shelf.
+    """
     want_to_read = already_read = 0
     if not urls:
         return want_to_read, already_read
@@ -18,6 +24,7 @@ async def add_books_to_reading_list(urls: list[str]):
         if random.choice([True, False]):
             res = await book_page.set_book_as_want_to_read()
             want_to_read += 1
+            # If the book was already in the "Already Read" shelf, it decreases the "Already Read" counter after it moved to "Want to Read" shelf
             if res == -1:
                 print_info(
                     f"Book at {url} was already in 'Already Read' shelf, skipping addition.")
@@ -25,6 +32,7 @@ async def add_books_to_reading_list(urls: list[str]):
         else:
             res = await book_page.set_book_as_already_read()
             already_read += 1
+            # If the book was already in the "Want to Read" shelf, it decreases the "Want to Read" counter after it moved to "Already Read" shelf
             if res == -1:
                 print_info(
                     f"Book at {url} was already in 'Want to Read' shelf, skipping addition.")
